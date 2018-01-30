99dc32437f5b6a162c6a76208ce4727ead2d38e3
Amir <jewelcl@gmail.com>
2018-01-30 21:24:34 +0000

    get data from json file
    json data visible in devtools. not rendered to page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { TourService} from "./services/services";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[TourService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+  
+  tours=[];
+
   title = 'app';
+
+  constructor(private _tourService: TourService){}
+  ngOnInit()
+  {   
+    this.tours=[];
+    console.log(this.title);
+    this._tourService.getAll()
+    .subscribe(
+      (data: any) => {
+          this.tours = data;
+          console.log(this.tours) ;
+      },
+      err => console.log(err), // error
+      () => console.log('getUserStatus Complete') // complete
+  );
+
+}
 }
--- src/app/services/tour/tour.service.ts
+++ src/app/services/tour/tour.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 import { CommonService } from "../shared/common.service";
 
 @Injectable()
 export class TourService {
     private data: any;
     private str: string;
-
+    private _url:string="https://my-json-server.typicode.com/tagdevteam/AssignmentApi/tours";    
     constructor(public http: Http,
                 public commonService: CommonService) {
     }
@@ -18,6 +19,8 @@ export class TourService {
     }
 
     getAll() {
+        return this.http.get(this._url)
+                  .map((response)=>response.json());
         
     }    
 }
